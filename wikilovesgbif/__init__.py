3e8f172f804bb3a701c5a5069e34333a4fc91495
ksledz <kamila.ktos.sledz@gmail.com>
2024-05-28 12:57:53 +0200

    /obs/id fnctionality

--- wikilovesgbif/__init__.py
+++ wikilovesgbif/__init__.py
@@ -4,6 +4,7 @@ from flask import render_template
 import re
 import requests
 
+placeholder = "check value"
 
 app = Flask(__name__)
 
@@ -33,7 +34,7 @@ class Observation:
         description += '%0A[[Category:Media from GBIF]]%0A[[Category:' + self.species + ']]'
         dest = 'https://commons.wikimedia.org/wiki/Special:Upload'
         res = dest + '?wpUploadDescription=' + description + '&wpLicense=cc-by-4.0&wpDestFile='
-        target_name = self.species + " GBIF observation " + str(self.key) + " " + str(len(self.urls))
+        target_name = self.species + " GBIF observation " + str(self.key) + " " + str(len(self.urls)) + "." + url.split(".")[-1]
         res += target_name + '&wpSourceType=url&wpUploadFileURL=' + url
         self.uploads.append(res)
     def __str__(self):
@@ -51,6 +52,44 @@ class Observation:
     def zip_urls(self):
         self.zipped = zip(self.urls, self.uploads)
 
+
+def objectify_result(result):
+    eventDate = placeholder
+    eventAuthor = placeholder
+    eventSpecies = placeholder
+    eventKey = placeholder
+    if 'eventDate' in result:
+        eventDate = result['eventDate']
+    if 'key' in result:
+        eventKey = result['key']
+    if 'species' in result:
+        eventSpecies = result['species']
+    if 'recordedBy' in result:
+        eventAuthor = result['recordedBy']
+    o = Observation (eventKey, eventSpecies, eventAuthor, eventDate)
+    if result['media'] != []:
+        if result['extensions'] != {}:
+            url1 = 'http://rs.gbif.org/terms/1.0/Multimedia'
+            url2 = 'http://rs.tdwg.org/ac/terms/Multimedia'
+            subUrl1 = 'http://purl.org/dc/terms/identifier'
+            subUrl2 = 'http://rs.tdwg.org/ac/terms/accessURI'
+            if url1 in result['extensions']:
+                for med in result['extensions'][url1]:
+                    o.add_url(med[subUrl1])
+                    o.add_upload(med[subUrl1])
+            if url2 in result['extensions']:
+                for med in result['extensions'][url2]:
+                    o.add_url(med[subUrl2])
+                    o.add_upload(med[subUrl2])
+
+        else:
+            for med in result['media']:
+                if 'identifier' in med:
+                    o.add_url(med['identifier'])
+                    o.add_upload(med['identifier'])
+        o.zip_urls()
+    return o
+
 def get_observations(gbif_id):
     observations = []
     url='https://api.gbif.org/v1/occurrence/search?acceptedTaxonKey='+str(gbif_id)+'&license=CC_BY_4_0'
@@ -58,45 +97,23 @@ def get_observations(gbif_id):
     response = requests.get(url).json()
     for result in response['results']:
         if result['media'] != []:
-            eventDate = "check manually"
-            eventAuthor = eventDate
-            eventSpecies = eventDate
-            eventKey = eventDate
-            if 'eventDate' in result:
-                eventDate = result['eventDate']
-            if 'key' in result:
-                eventKey = result['key']
-            if 'species' in result:
-                eventSpecies = result['species']
-            if 'recordedBy' in result:
-                eventAuthor = result['recordedBy']
-            o = Observation (eventKey, eventSpecies, eventAuthor, eventDate)
-            if result['extensions'] != {}:
-                url1 = 'http://rs.gbif.org/terms/1.0/Multimedia'
-                url2 = 'http://rs.tdwg.org/ac/terms/Multimedia'
-                subUrl1 = 'http://purl.org/dc/terms/identifier'
-                subUrl2 = 'http://rs.tdwg.org/ac/terms/accessURI'
-                if url1 in result['extensions']:
-                    for med in result['extensions'][url1]:
-                        o.add_url(med[subUrl1])
-                        o.add_upload(med[subUrl1])
-                if url2 in result['extensions']:
-                    for med in result['extensions'][url2]:
-                        o.add_url(med[subUrl2])
-                        o.add_upload(med[subUrl2])
-
-            else:
-                for med in result['media']:
-                    if 'identifier' in med:
-                        o.add_url(med['identifier'])
-                        o.add_upload(med['identifier'])
-            o.zip_urls()
-            observations.append(o)
+            observations.append(objectify_result(result))
     return observations
 
 
-@app.route("/res/<question_id>")
-def show_results(question_id):
-    objects = get_observations(question_id)
+@app.route("/res/<species_id>")
+def show_results(species_id):
+    objects = get_observations(species_id)
     return render_template('results.html', objects=objects)
 
+def get_observation(obs_id):
+    observations = []
+    url='https://api.gbif.org/v1/occurrence/'+str(obs_id)
+    response = requests.get(url).json()
+    observations.append(objectify_result(response))
+    return observations
+
+@app.route("/obs/<observation_id>")
+def show_observation(observation_id):
+    objects = get_observation(observation_id)
+    return render_template('results.html', objects=objects)
